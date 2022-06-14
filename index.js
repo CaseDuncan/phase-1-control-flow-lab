778b2ea60617259e32737cc773b78ed1c42d0e13
devdan <oluochdancun19@gmail.com>
2022-06-14 15:47:46 +0300

    commit tree fe80703a87289bb927d0ea8faf6d48483c4e7e22

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  // Write your code here!
+  if(distance<400){
+    return "This one is on me!"
+  }
+  else if(distance >200){
+    return 'I will gladly take your thirty bucks.'
+  
+  }
+  else if(distance >2500){
+    return 'No can do.'
+  }
+}
+scuberGreetingForFeet(199, 2001, 2501)
+
+function ternaryCheckCity(city){
+  if(city === ' NYC'){
+    return "Ok, sounds good."
+  }
+  else{
+    return "No go.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(comment){
   // Write your code here!
+  if(comment ==="generous"){
+    return "Thank you so much."
+  }
+  // else if(comment !="generous"){
+  //   return "Thank you so much."
+  // }
+  else{
+    return "Bye.";
+  }
 }
